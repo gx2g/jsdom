ae313236f01d9aedd45bd63ad1c6950b6d9c0e97
Robert Resendez <grafx2g@gmail.com>
2019-06-10 01:14:38 -0700

    comment out

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // EXAMINE THE DOCUMENT OBJECT //
-console.dir(document);
-
-// console.log(document.domain);
-// console.log(document.URL);
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
 // console.log(document.title);
 // //document.title =  123;
 // console.log(document.doctype);
